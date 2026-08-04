547be300c34fc177c62c0227c49f8735c28517e0
NathaliaValentinaCardozaAzuaje <nvcardozaa@eafit.edu.co>
2026-08-03 23:41:03 -0500

    Cambios realizados segun tutorial 2

--- fullstack/src/controllers/HomeController.ts
+++ fullstack/src/controllers/HomeController.ts
@@ -1,4 +1,5 @@
 import type { Request, Response } from 'express'; 
+import { books, findBookById } from '../data/Books.js';
  
 export class HomeController { 
   static index(req: Request, res: Response): void {
@@ -21,4 +22,30 @@ export class HomeController {
 
     res.render('home/contact', { viewData: viewData });
   }
+
+  static Main_Point(req: Request, res: any) { 
+    const viewData: any = {}; 
+     viewData["books"] = books; 
+    res.render('home/books', viewData); 
+  } 
+
+  static show(req: Request, res: Response): void  
+  { 
+    const id = Number(req.params.id);
+
+    if (Number.isNaN(id)) {
+      res.status(400).render('home/index', { viewData: { title: 'Invalid book id' } });
+      return;
+    }
+
+    const book = findBookById(id);
+
+    if (!book) {
+      res.status(404).render('home/index', { viewData: { title: 'Book not found' } });
+      return;
+    }
+
+    res.render('home/show', { book: book }) 
+  } 
+  
 }
---
+++ fullstack/src/models/Book.ts
@@ -0,0 +1,9 @@
+export class Book { 
+  constructor( 
+    public id: number, 
+    public title: string, 
+    public category: string, 
+    public price: number, 
+    public stock: number 
+  ) { } 
+} 
